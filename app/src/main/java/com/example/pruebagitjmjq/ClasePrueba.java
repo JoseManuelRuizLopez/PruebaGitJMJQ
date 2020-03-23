8d0014dd2a93398b6f65a9e283db3882dec0e56f
José Francisco Quintero Moreno <josequinteromoreno13@gmail.com>
2020-03-23 11:25:51 +0100

    Tamo, comiteando foto

---
+++ app/src/main/java/com/example/pruebagitjmjq/ClasePrueba.java
@@ -0,0 +1,14 @@
+package com.example.pruebagitjmjq;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ClasePrueba extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clase_prueba);
+    }
+}
